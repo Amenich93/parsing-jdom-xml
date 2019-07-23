311e57986aaecf18695fa9d96485bf20fb8b3b6b
chebbi ameni <chebbi.ameni93@gmail.com>
2019-07-23 18:23:52 +0200

    new modif

---
+++ parsing-jdom-xml/src/main/java/entities1/Product.java
@@ -0,0 +1,80 @@
+package entities1;
+
+
+public class Product {
+	private String MasseOrdmaMin;
+	private String MasseOrdmaAffine;
+	private String MasseOrdmaEssi1Min;
+	private String MasseOrdmaEssi1Affin;
+	private String MasseOrdmaEssi2Min;
+	private String MasseOrdmaEssi2Affine;
+	private String Co2UrbCarb1;
+	private String Co2ExurbCarb1;
+	public String getMasseOrdmaMin() {
+		return MasseOrdmaMin;
+	}
+	public void setMasseOrdmaMin(String masseOrdmaMin) {
+		MasseOrdmaMin = masseOrdmaMin;
+	}
+	public String getMasseOrdmaAffine() {
+		return MasseOrdmaAffine;
+	}
+	public void setMasseOrdmaAffine(String masseOrdmaAffine) {
+		MasseOrdmaAffine = masseOrdmaAffine;
+	}
+	public String getMasseOrdmaEssi1Min() {
+		return MasseOrdmaEssi1Min;
+	}
+	public void setMasseOrdmaEssi1Min(String masseOrdmaEssi1Min) {
+		MasseOrdmaEssi1Min = masseOrdmaEssi1Min;
+	}
+	public String getMasseOrdmaEssi1Affin() {
+		return MasseOrdmaEssi1Affin;
+	}
+	public void setMasseOrdmaEssi1Affin(String masseOrdmaEssi1Affin) {
+		MasseOrdmaEssi1Affin = masseOrdmaEssi1Affin;
+	}
+	public String getMasseOrdmaEssi2Min() {
+		return MasseOrdmaEssi2Min;
+	}
+	public void setMasseOrdmaEssi2Min(String masseOrdmaEssi2Min) {
+		MasseOrdmaEssi2Min = masseOrdmaEssi2Min;
+	}
+	public String getMasseOrdmaEssi2Affine() {
+		return MasseOrdmaEssi2Affine;
+	}
+	public void setMasseOrdmaEssi2Affine(String masseOrdmaEssi2Affine) {
+		MasseOrdmaEssi2Affine = masseOrdmaEssi2Affine;
+	}
+	public String getCo2UrbCarb1() {
+		return Co2UrbCarb1;
+	}
+	public void setCo2UrbCarb1(String co2UrbCarb1) {
+		Co2UrbCarb1 = co2UrbCarb1;
+	}
+	public String getCo2ExurbCarb1() {
+		return Co2ExurbCarb1;
+	}
+	public void setCo2ExurbCarb1(String co2ExurbCarb1) {
+		Co2ExurbCarb1 = co2ExurbCarb1;
+	}
+	public Product(String masseOrdmaMin, String masseOrdmaAffine, String masseOrdmaEssi1Min, String masseOrdmaEssi1Affin,
+			String masseOrdmaEssi2Min, String masseOrdmaEssi2Affine, String co2UrbCarb1, String co2ExurbCarb1) {
+		super();
+		MasseOrdmaMin = masseOrdmaMin;
+		MasseOrdmaAffine = masseOrdmaAffine;
+		MasseOrdmaEssi1Min = masseOrdmaEssi1Min;
+		MasseOrdmaEssi1Affin = masseOrdmaEssi1Affin;
+		MasseOrdmaEssi2Min = masseOrdmaEssi2Min;
+		MasseOrdmaEssi2Affine = masseOrdmaEssi2Affine;
+		Co2UrbCarb1 = co2UrbCarb1;
+		Co2ExurbCarb1 = co2ExurbCarb1;
+	}
+	public Product() {
+		super();
+	}
+	
+	
+}
+	
+	
